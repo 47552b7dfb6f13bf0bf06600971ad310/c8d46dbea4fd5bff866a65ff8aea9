c466eb924d006a850b71d1aadde4cc811ecdf593
Your Name <you@example.com>
2025-07-04 00:10:20 +0700

    Update

--- src/server/api/rank/manage/process/send.post.ts
+++ src/server/api/rank/manage/process/send.post.ts
@@ -8,8 +8,9 @@ export default defineEventHandler(async (event) => {
     const { _id } = await readBody(event)
     if(!_id) throw 'Dữ liệu đầu vào không hợp lệ'
 
-    const processEvent = await DB.GameRankProcess.findOne({ _id: _id }).select('server type') as IDBGameRankProcess
+    const processEvent = await DB.GameRankProcess.findOne({ _id: _id }).select('server type send') as IDBGameRankProcess
     if(!processEvent) throw 'Tiến trình không tồn tại'
+    if(!!processEvent.send) throw 'Phần quà đã được trao'
 
     await rankProcessAward(processEvent.server) 
 
